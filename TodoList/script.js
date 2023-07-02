2f80e385f39a1d695f84582609273db4d9a76fe4
kelwinxd <kelwin_esechiel@hotmail.com>
2023-07-01 23:24:07 -0300

    up

--- TodoList/script.js
+++ TodoList/script.js
@@ -2,6 +2,8 @@ const sub = document.querySelector(".sub")
 const input = document.querySelector("#text")
 const listOut = document.querySelector('.output')
 const containerEdit = document.querySelector('.editContainer')
+const wall = document.querySelector('.wall')
+const save = document.querySelector('.save')
 
 
 sub.addEventListener('click', (e) => {
@@ -43,9 +45,45 @@ Edit.addEventListener('click', () => {
     
     inputEdit.removeAttribute("readonly")
     containerEdit.classList.add('block')
+    containerEdit.style.display = 'block'
+    inputEdit.style.marginTop = '50px'
     containerEdit.appendChild(inputEdit)
+    wall.style.display = 'block'
+
+
 
 })
+
+save.addEventListener('click', () => {
+
+    if(!(inputEdit.value == '')){
+        inputEdit.setAttribute('readonly','readonly')
+        inputEdit.style.marginTop = '0'
+       containerEdit.classList.add('exit')
+        setTimeout(() => {
+            Task.appendChild(inputEdit)
+            wall.style.display = 'none'
+            containerEdit.style.display = 'none'
+        },400)
+
+        setTimeout(() => {
+         containerEdit.classList.remove('exit')
+        },600)
+       
+    } else {
+        alert('Please enter a task!')
+    }
+  
+    
+
+
+
+})
+
+Del.addEventListener('click',() => {
+    listOut.removeChild(Task)
+})
+
 
 
 
