b7296981a3b80e42165f947fda888bfb08f251db
kelwinxd <kelwin_esechiel@hotmail.com>
2023-08-11 18:37:43 -0300

    up

--- All/script.js
+++ All/script.js
@@ -5,6 +5,7 @@ const containerModal = document.querySelector('.mod-container')
 const closeM = document.querySelector('.close')
 
 
+
 btnDrop.addEventListener('click', () => {
    drop.classList.toggle('ativo')
 })
@@ -43,37 +44,79 @@ function outModal(event){
  }
 
 
+
 function animaScroll(){
-    const scrollItems = document.querySelectorAll('.scroll')
+    const scrollItems = document.querySelectorAll('.js-scroll')
     if(scrollItems.length){
         scrollItems.forEach((s) => {
             
             const topSec = s.getBoundingClientRect().top
-            const isVisible = ((topSec + 1) - (window.innerHeight * 0.6)) < 0
+            const isVisible = ((topSec + 200) - (window.innerHeight)) < 0
             
 
            if(isVisible){
-            s.classList.add('ativo')
+            s.classList.add('ativo2')
            } else {
-            s.classList.remove('ativo')
+            s.classList.remove('ativo2')
            }
 
         })
     }
 
-
-    
-
-
-
-
-
-
-    
 }
 window.addEventListener('scroll', animaScroll)
 animaScroll()
 
+
+const btnFetch = document.querySelector('#btnS')
+const divTemp = document.querySelector(".showT")
+
+function fetchApi(event){
+    event.preventDefault()
+    const City = document.querySelector('#input1').value.toString()
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(City)}&appid=7e9475cacf35b9da97d63abdb838ec1a`).then(r => r.json())
+    .then(r => {
+        console.log(r)
+       document.querySelector('#temp').textContent = +(r.main.temp - 273).toFixed(1) + ' °C'
+      if(r.weather.main == "Clear"){
+        console.log("Claro")
+
+      }
+      function conditionW(main){
+        const img = document.querySelector("#img")
+        
+       switch (main) {
+        case "Clouds":
+            img.src = "./clouds.png"
+            return "Nublado"
+            
+            
+        case "Clear":
+            img.src = "./sunny.png"
+             return "Ensolarado"
+        default:
+            break;
+       }
+
+       
+      }
+       document.querySelector('#main').textContent = conditionW(r.weather[0].main)
+      
+        
+
+    }).catch(rej => {
+        document.querySelector('#temp').textContent = ""
+        document.querySelector('#main').textContent = ""
+        divTemp.removeChild(img)
+        const errorM = document.createElement("h2")
+        errorM.classList.add('error')
+        errorM.innerText= 'Não encontrado'
+        divTemp.appendChild(errorM)
+    })
+    
+}
+
+btnFetch.addEventListener('click', fetchApi)
 
 
 
@@ -82,4 +125,12 @@ animaScroll()
 closeM.addEventListener('click',closeModal)
 modal.addEventListener('click', modalOpen)
 containerModal.addEventListener('click', outModal)
+
+
+
+
+
+
+
+
 
